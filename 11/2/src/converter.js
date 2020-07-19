743f855618cf1fec4e970f7b8424f09691cd4f1f
ynonp <ynon@ynonperek.com>
2020-07-19 21:10:16 +0300

    Update converter.js

--- 11/2/src/converter.js
+++ 11/2/src/converter.js
@@ -3,6 +3,7 @@ import React, {useEffect, useState} from 'react';
 import '../css/main.css';
 
 export default function () {
+    // Can you use only one state variable? Do you think you should?
     const [m, setM] = useState(0);
     const [s, setS] = useState(0);
     const [h, setH] = useState(0);
@@ -12,19 +13,21 @@ export default function () {
         setM(secunds/60);
         setS(secunds);
     }
+    // Please don't use id inside JSX
+    // If anyone will create multiple instances of this component, they'll get multiple elements with the same id    
     return (
         <>
             <div>
                 <label htmlFor="h">שעות</label>
-                <input type="number" id="h" value={h} onChange={(e) => converter(e.target.value*3600)}/>
+                <input type="text" id="h" value={h} onChange={(e) => converter(e.target.value*3600)}/>
             </div>
             <div>
                 <label htmlFor="m">דקות</label>
-                <input type="number" id="m" value={m} onChange={(e) => converter(e.target.value*60)}/>
+                <input type="text" id="m" value={m} onChange={(e) => converter(e.target.value*60)}/>
             </div>
             <div>
                 <label htmlFor="s">שניות</label>
-                <input type="number" id="s" value={s} onChange={(e) => converter(e.target.value)}/>
+                <input type="text" id="s" value={s} onChange={(e) => converter(e.target.value)}/>
             </div>
         </>
     )
