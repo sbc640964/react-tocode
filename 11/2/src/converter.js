c4a37e8eb2144f17d5efc4a67d18b083dcbbccf2
simcha <sbc640964@gmail.com>
2020-07-19 20:53:04 +0300

    fix 11 -2

--- 11/2/src/converter.js
+++ 11/2/src/converter.js
@@ -1,36 +1,30 @@
 import React, {useEffect, useState} from 'react';
 
+import '../css/main.css';
 
 export default function () {
     const [m, setM] = useState(0);
     const [s, setS] = useState(0);
     const [h, setH] = useState(0);
 
-    useEffect(()=>{
-        setS(m*60);
-    }, [m]);
-
-    useEffect(()=>{
-        setH(s/3600);
-    }, [s]);
-
-    useEffect(()=>{
-        setM(h*60);
-    }, [h]);
-
+    function converter(secunds) {
+        setH(secunds/3600);
+        setM(secunds/60);
+        setS(secunds);
+    }
     return (
         <>
             <div>
                 <label htmlFor="h">שעות</label>
-                <input type="text" id="h" value={h} onChange={(e) => setH(e.target.value)}/>
+                <input type="text" id="h" value={h} onChange={(e) => converter(e.target.value*3600)}/>
             </div>
             <div>
                 <label htmlFor="m">דקות</label>
-                <input type="text" id="m" value={m} onChange={(e) => setM(e.target.value)}/>
+                <input type="text" id="m" value={m} onChange={(e) => converter(e.target.value*60)}/>
             </div>
             <div>
                 <label htmlFor="s">שניות</label>
-                <input type="text" id="s" value={s} onChange={(e) => setS(e.target.value)}/>
+                <input type="text" id="s" value={s} onChange={(e) => converter(e.target.value)}/>
             </div>
         </>
     )
