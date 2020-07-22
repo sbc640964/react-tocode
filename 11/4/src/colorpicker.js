4b282498e7852f45a5a8a26f35bf6b54f050b70f
simcha <sbc640964@gmail.com>
2020-07-22 18:29:42 +0300

    תרגיל 4

---
+++ 11/4/src/colorpicker.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import tinycolor from 'tinycolor2';
+
+import '../css/main.css';
+export default function ({ color }) {
+    const [currentColor, setCurrentColor] = useState(color);
+
+    function style(){
+
+        const _currentColor = tinycolor(currentColor);
+
+        const style = {
+            background:currentColor,
+            color: _currentColor.isDark() ?
+                _currentColor.lighten(20) :
+                _currentColor.darken(20),
+        }
+
+        return style;
+    }
+
+
+    return(
+        <div className="container">
+           <input type="color" value={currentColor} onChange={(e) => setCurrentColor(e.target.value)}/>
+           <div className="result" style={style()}>
+               Result
+           </div>
+        </div>
+    )
+}
--- 11/4/src/main.js
+++ 11/4/src/main.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
+import ColorPicker from './colorpicker';
 const App = () => {
 
   return (
-    ''
+      <ColorPicker/>
   )
 };
 
